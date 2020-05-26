08c9dd96e201b0f26cda049ae4e1b16281844bca
1fbff5f83b23d39d38b1dfcb4cac8d9b <0a02d0db@opayq.com>
2020-05-26 21:48:10 +0200

    downgrade to python 3.6

--- auth_gateway/agw/serializers.py
+++ auth_gateway/agw/serializers.py
@@ -1,19 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_registration.api.serializers import _build_initial_user
+from rest_registration.utils.users import build_initial_user
 from django.contrib.auth.password_validation import validate_password
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = get_user_model()
 		fields = (
-			'username',
-			'email',
-			'password',
+		    'username',
+		    'email',
+		    'password',
 		)
 
 	def validate_password(self, password):
-		user = _build_initial_user(self.initial_data)
+		user = build_initial_user(self.initial_data)
 		validate_password(password, user=user)
 		return password
 
